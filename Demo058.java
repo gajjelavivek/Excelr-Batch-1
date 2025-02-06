8e72152a34a06348e136d6736d9ca507b4b7f102
gajjelavivek <gajjelavivek301@gmail.com>
2025-02-06 10:57:36 +0530

    To print factors of numbers

---
+++ Demo058.java
@@ -0,0 +1,20 @@
+public class Demo058 {
+
+    public static void printFactors(int number) {
+        System.out.print("Factors of " + number + " are: ");
+        
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println(); 
+    }
+
+    public static void main(String[] args) {
+      
+        int number = 12; 
+        
+        printFactors(number);
+    }
+}
